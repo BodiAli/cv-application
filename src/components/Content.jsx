38000bea34c81c691e126d93e29b5ab723d462b0
Bodi_Ali <bodiali59@gmail.com>
2024-07-04 09:22:35 +0300

    Pass props to section to handle input change

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,27 @@
 import Section from "./Section";
-import formInformation from "../data/information";
 import "../styles/Content.css";
 
-function Content() {
+//TODO: Make hamburger button and make the resume come down from the top
+
+function Content({ formData, onInputChange }) {
   return (
-    <main>
-      {formInformation.map((info) => {
-        return <Section key={info.id} information={info} />;
-      })}
-    </main>
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="view-cv">
+            <h2>Click To View CV Add</h2>
+            <button>ADD</button>
+          </div>
+        </div>
+      </header>
+      <main className="content">
+        <div className="container">
+          {formData.map((section) => {
+            return <Section key={section.id} section={section} onInputChange={onInputChange} />;
+          })}
+        </div>
+      </main>
+    </>
   );
 }
 
