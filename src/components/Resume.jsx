24a7353d03edc574c08fa1886e0b7ee4212f518a
Bodi_Ali <bodiali59@gmail.com>
2024-07-04 09:23:11 +0300

    Get data from updated form data

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,3 +1,28 @@
-function Resume() {}
+import "../styles/Resume.css";
+
+function Resume({ formData }) {
+  return (
+    <div className="resume">
+      <div className="container">
+        {formData.map((section) => {
+          return (
+            <div className="resume-container" key={section.id}>
+              <h2>{section.title}</h2>
+              <ul>
+                {section.fields.map((field) => {
+                  return (
+                    <li key={field.id}>
+                      {field.label}: {field.value}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default Resume;
