840bce41275d95e755ba42018f211eea76821d0f
Bodi_Ali <bodiali59@gmail.com>
2024-07-04 17:58:33 +0300

    Initial structure of the resume and handle className when button is clicked

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,26 +1,49 @@
 import "../styles/Resume.css";
 
-function Resume({ formData }) {
-  return (
-    <div className="resume">
-      <div className="container">
-        {formData.map((section) => {
-          return (
-            <div className="resume-container" key={section.id}>
-              <h2>{section.title}</h2>
-              <ul>
-                {section.fields.map((field) => {
-                  return (
-                    <li key={field.id}>
-                      {field.label}: {field.value}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          );
-        })}
+function Resume({ formData, isActive }) {
+  let details;
+  const resumeData = formData.map((section) => {
+    if (section.title === "Personal Details") {
+      let fullName = "";
+
+      {
+        const personalInfo = section.fields.map((field) => {
+          if (field.label === "Full Name") {
+            fullName = field.value;
+            return null;
+          } else {
+            return <p key={field.id}>{field.value}</p>;
+          }
+        });
+
+        const filteredPersonalInfo = personalInfo.filter((info) => info !== null);
+        details = (
+          <div className="personal-details">
+            <h2>{fullName}</h2>
+            <div className="personal-info">{filteredPersonalInfo}</div>
+          </div>
+        );
+      }
+    } else {
+      details = (
+        <div className="experience-container">
+          {section.fields.map((field) => {
+            return <p key={field.id}>{field.value}</p>;
+          })}
+        </div>
+      );
+    }
+    return (
+      <div className="resume-container" key={section.id}>
+        <h2>{section.title}</h2>
+        {details}
       </div>
+    );
+  });
+
+  return (
+    <div className={`resume ${isActive ? "visible" : "hidden"}`}>
+      <div className="container">{resumeData}</div>
     </div>
   );
 }
