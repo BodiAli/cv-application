f3a44a2f1e6bb6064db68645b3dd8dc51af75645
Bodi_Ali <bodiali59@gmail.com>
2024-07-04 09:21:58 +0300

    Handle input change to update resume content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import Content from "./components/Content";
 import Resume from "./components/Resume";
+import formInformation from "./data/information";
 import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState(formInformation);
+
+  function handleInputChange(sectionId, fieldId, value) {
+    const updatedFormData = formData.map((section) => {
+      if (section.id === sectionId) {
+        const updatedFields = section.fields.map((field) => {
+          if (field.id === fieldId) {
+            return { ...field, value };
+          }
+          return field;
+        });
+        return { ...section, fields: updatedFields };
+      }
+      return section;
+    });
+    setFormData(updatedFormData);
+  }
+
   return (
     <>
-      <Content />
-      <Resume />
+      <Content formData={formData} onInputChange={handleInputChange} />
+      <Resume formData={formData} />
     </>
   );
 }
