c388b1b40aa53f0469c5f5cacc6ee4616fae9beb
Bodi_Ali <bodiali59@gmail.com>
2024-07-04 17:56:54 +0300

    Handle click to view CV

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,17 @@
 import Section from "./Section";
 import "../styles/Content.css";
 
-//TODO: Make hamburger button and make the resume come down from the top
 
-function Content({ formData, onInputChange }) {
+function Content({ formData, onInputChange, onButtonClick, isActive }) {
   return (
     <>
       <header className="header">
-        <div className="container">
-          <div className="view-cv">
-            <h2>Click To View CV Add</h2>
-            <button>ADD</button>
+        <div className="header-container">
+          <div onClick={onButtonClick} className="view-cv">
+            <h2>Click To View CV</h2>
+            <button className={"hamburger " + (isActive ? "is-active" : "")}>
+              <div className="bar"></div>
+            </button>
           </div>
         </div>
       </header>
