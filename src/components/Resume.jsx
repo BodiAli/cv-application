d8094f389e897097c325e1644eb5e001578670c6
Bodi_Ali <bodiali59@gmail.com>
2024-07-05 11:14:57 +0300

    Show experience data from their there respective arrays

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,49 +1,81 @@
+import emailIcon from "../assets/email.svg";
+import phoneIcon from "../assets/phone.svg";
+import locationIcon from "../assets/location.svg";
 import "../styles/Resume.css";
 
-function Resume({ formData, isActive }) {
-  let details;
+//TODO: style resume and add an edit functionality
+
+function Resume({ formData, isActive, experienceData, educationData }) {
   const resumeData = formData.map((section) => {
     if (section.title === "Personal Details") {
       let fullName = "";
 
-      {
-        const personalInfo = section.fields.map((field) => {
-          if (field.label === "Full Name") {
-            fullName = field.value;
-            return null;
-          } else {
-            return <p key={field.id}>{field.value}</p>;
-          }
-        });
+      const personalInfo = section.fields.map((field) => {
+        if (field.label === "Full Name") {
+          fullName = field.value;
+          return null;
+        } else {
+          return (
+            <div className="personal-icon-container" key={field.id}>
+              <img
+                src={
+                  field.label === "Email"
+                    ? emailIcon
+                    : field.label === "Phone Number"
+                    ? phoneIcon
+                    : locationIcon
+                }
+                alt=""
+              />
+              <p>{field.value}</p>
+            </div>
+          );
+        }
+      });
 
-        const filteredPersonalInfo = personalInfo.filter((info) => info !== null);
-        details = (
-          <div className="personal-details">
-            <h2>{fullName}</h2>
-            <div className="personal-info">{filteredPersonalInfo}</div>
-          </div>
-        );
-      }
-    } else {
-      details = (
-        <div className="experience-container">
-          {section.fields.map((field) => {
-            return <p key={field.id}>{field.value}</p>;
-          })}
+      const filteredPersonalInfo = personalInfo.filter((info) => info !== null);
+      return (
+        <div key={section.id} className="personal-details">
+          {fullName && <h2>{fullName}</h2>}
+          <div className="personal-info">{filteredPersonalInfo}</div>
         </div>
       );
     }
-    return (
-      <div className="resume-container" key={section.id}>
-        <h2>{section.title}</h2>
-        {details}
-      </div>
-    );
   });
+
+  const educationElement = (
+    <div className="experience-container">
+      {educationData.map((section) => {
+        return section.map((field) => {
+          return <p key={field.id}>{field.value}</p>;
+        });
+      })}
+    </div>
+  );
+
+  const experienceElement = (
+    <div className="experience-container">
+      {experienceData.map((section) => {
+        return section.map((field) => {
+          return <p key={field.id}>{field.value}</p>;
+        });
+      })}
+    </div>
+  );
 
   return (
     <div className={`resume ${isActive ? "visible" : "hidden"}`}>
-      <div className="container">{resumeData}</div>
+      <div className="resume-container">
+        <div className="resume-section-container">{resumeData}</div>
+        <div className="resume-section-container">
+          <h2>Education</h2>
+          {educationElement}
+        </div>
+        <div className="resume-section-container">
+          <h2>Experience</h2>
+          {experienceElement}
+        </div>
+      </div>
     </div>
   );
 }
