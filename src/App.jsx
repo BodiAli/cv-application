93fe96e56d51a38cd96323d6a219fa1855849a8b
Bodi_Ali <bodiali59@gmail.com>
2024-07-06 13:10:02 +0300

    lift listElements state up to the root components add functionality to delete listElements when a form submits it will reinitialize the input values

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [educationData, setEducationData] = useState(educationArray);
   const [experienceData, setExperienceData] = useState(experienceArray);
   const [isActive, setIsActive] = useState(false);
+  const [listElements, setListElements] = useState({});
 
   function onButtonClick() {
     setIsActive(!isActive);
@@ -30,12 +31,46 @@ function App() {
     setFormData(updatedFormData);
   }
 
+  function handleDeleteButton(buttonField, value) {
+    const updatedData = formData.map((section) => {
+      const updatedFields = section.fields.map((field) => {
+        if (field.id === buttonField.id) {
+          const newArr = [...field.arr];
+          const index = newArr.indexOf(value);
+          newArr.splice(index, 1);
+          return { ...field, arr: [...newArr] };
+        }
+
+        return field;
+      });
+      return { ...section, fields: updatedFields };
+    });
+
+    updatedData.forEach((section) => {
+      section.fields.forEach((field) => {
+        if (field.id === buttonField.id) {
+          setListElements((prevListElements) => ({
+            ...prevListElements,
+            [buttonField.id]: field.arr,
+          }));
+        }
+      });
+    });
+
+    setFormData(updatedData);
+  }
+
   function handleFormSubmit(ev) {
     ev.preventDefault();
     formData.forEach((section) => {
       function updateStates(section, stateVariable, setStateVariable) {
-        const fieldValues = section.fields.map((field) => ({ value: field.value, id: field.id }));
-
+        const fieldValues = section.fields.map((field) => {
+          if (field.arr) {
+            return { value: field.arr, id: field.id };
+          } else {
+            return { value: field.value, id: field.id };
+          }
+        });
         const isDuplicate = stateVariable.some((data) => {
           return (
             data.length === fieldValues.length &&
@@ -43,19 +78,22 @@ function App() {
               if (!Array.isArray(obj.value)) {
                 return obj.value === fieldValues[index].value;
               } else if (Array.isArray(obj.value)) {
-                return (
-                  obj.value.length === fieldValues[index].value.length &&
-                  obj.value.every((string, index) => {
-                    return string === fieldValues[index].value[index];
-                  })
-                );
+                if (obj.value.length !== fieldValues[index].value.length) {
+                  return false;
+                } else if (obj.value.length === 0 && fieldValues[index].value.length === 0) {
+                  return true;
+                } else {
+                  return obj.value.every((string, index2) => {
+                    return string === fieldValues[index].value[index2];
+                  });
+                }
               }
             })
           );
         });
 
         if (!isDuplicate) {
-          setStateVariable((prevEducationData) => [...prevEducationData, fieldValues]);
+          setStateVariable((prevStateVariable) => [...prevStateVariable, fieldValues]);
         }
       }
 
@@ -75,9 +113,24 @@ function App() {
       return {
         ...section,
         fields: section.fields.map((field) => {
-          return { ...field, value: "" };
+          if (field.arr) {
+            return { ...field, value: "", arr: [] };
+          } else {
+            return { ...field, value: "" };
+          }
         }),
       };
+    });
+
+    initializeFormInputs.forEach((section) => {
+      section.fields.forEach((field) => {
+        if (field.arr) {
+          setListElements((prevListElements) => ({
+            ...prevListElements,
+            [field.id]: field.arr,
+          }));
+        }
+      });
     });
 
     setFormData(initializeFormInputs);
@@ -85,14 +138,45 @@ function App() {
     // console.log(experienceData);
   }
 
+  function onAddListClick(buttonField) {
+    const updatedData = formData.map((section) => {
+      const updatedFields = section.fields.map((field) => {
+        if (field.id === buttonField.id) {
+          if (!field.arr.includes(field.value) && field.value.trim() !== "") {
+            return { ...field, arr: [...field.arr, buttonField.value], value: "" };
+          }
+        }
+
+        return field;
+      });
+      return { ...section, fields: updatedFields };
+    });
+
+    updatedData.forEach((section) => {
+      section.fields.forEach((field) => {
+        if (field.id === buttonField.id) {
+          setListElements((prevListElements) => ({
+            ...prevListElements,
+            [buttonField.id]: field.arr,
+          }));
+        }
+      });
+    });
+
+    setFormData(updatedData);
+  }
+
   return (
     <>
       <Content
+        onDelete={handleDeleteButton}
         onFormSubmit={handleFormSubmit}
         isActive={isActive}
         onButtonClick={onButtonClick}
         formData={formData}
+        onAddListClick={onAddListClick}
         onInputChange={handleInputChange}
+        listElements={listElements}
       />
       <Resume
         educationData={educationData}
